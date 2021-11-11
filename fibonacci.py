6f3ea18e594121c4c1fef2c8d7df51484b9fdfee
Sumedh-Ghotekar <94127543+Sumedh-Ghotekar@users.noreply.github.com>
2021-11-11 22:15:23 +0530

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,21 @@
+#Fibonacci numbers
+n=int(input("Enter the no. of terms : "))
+n1=0
+n2=1
+k=0
+if n<=0:
+    print("Invalid Input")
+else:
+    for i in range(0,n):
+        print(k)
+        n1=n2
+        n2=k
+        k=n1+n2
+        
+        
+    
+        
+        
+        
+    
+        
