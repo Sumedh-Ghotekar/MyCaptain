6296a9c93bf90ee091aa406482e9d605b6cba7f0
Sumedh-Ghotekar <94127543+Sumedh-Ghotekar@users.noreply.github.com>
2021-11-13 11:05:36 +0530

    Add files via upload

---
+++ Sets.py
@@ -0,0 +1,7 @@
+x={0,1,3,4,5,9,10,24,65,87,34,72,6,4,76,89,2,}
+y={7,8,3,6,54,65,89,1,90,54,76,34}
+print("Union of x and y is: ",x.union(y))
+print("Intersection of x and y is: ",x.intersection(y))
+print("Difference of x and y is: ",x.difference(y))
+print("Symmetric differnce of x and y is: ",x.symmetric_difference(y))
+
